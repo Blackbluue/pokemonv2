a962a6b1f0ee5804d28402d191fdef88d929181d
Raymone Miller <miller_raymone@hotmail.com>
2023-05-27 18:23:20 -0400

    Add exp growth rate lookup table

--- sql/tables/static_tables.sql
+++ sql/tables/static_tables.sql
@@ -1,3 +1,11 @@
+CREATE TABLE Game(
+    id              TINYINT UNSIGNED   NOT NULL,
+    display_name    VARCHAR(16)        NOT NULL,
+    abbreviation    VARCHAR(5)         NOT NULL,
+    generation      TINYINT UNSIGNED   NOT NULL,
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE Type(
     name    VARCHAR(16)   NOT NULL,
     PRIMARY KEY(name)
@@ -9,15 +17,7 @@ CREATE TABLE TypeMatchup(
     multiplier DECIMAL(2, 1) NOT NULL DEFAULT 1,
     PRIMARY KEY(attacker, defender),
     FOREIGN KEY(attacker) REFERENCES Type(name),
-    FOREIGN KEY(defender) REFERENCES Type(names)
-);
-
-CREATE TABLE Game(
-    id              TINYINT UNSIGNED   NOT NULL,
-    display_name    VARCHAR(16)        NOT NULL,
-    abbreviation    VARCHAR(5)         NOT NULL,
-    generation      TINYINT UNSIGNED   NOT NULL,
-    PRIMARY KEY(id)
+    FOREIGN KEY(defender) REFERENCES Type(name)
 );
 
 -- TODO: move this table into pokedex_tables.sql
@@ -38,4 +38,9 @@ CREATE TABLE SpawnMethod(
     id      TINYINT UNSIGNED NOT NULL,
     method  TINYTEXT         NOT NULL,
     PRIMARY KEY(id)
+);
+
+CREATE TABLE ExpGrowth(
+    growth_rate VARCHAR(16) NOT NULL,
+    PRIMARY KEY(growth_rate)
 );
